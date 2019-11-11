fb8f96d573773f90b1eb3e8ea66ab88803bcedb1
Bas Peters <bas@github.com>
2019-11-11 19:02:06 +0100

    Change static folder

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 
-app.use(express.static('assets'));
+//app.use(express.static('assets'));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/" + "index.html");
