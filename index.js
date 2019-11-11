570e8619e489e4942e43447d95087f6da14b06d7
Bas Peters <bas@github.com>
2019-11-11 19:36:03 +0100

    Few changes

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + "/" + "index.html");
 })
 
-app.get('/about', function (req, res) {
-    res.send('About page. Nice.')
-})
-
 //Launch listening server on port 8080
 var server = app.listen(8080, function () {
     console.log('Server listening on port 8080!')
